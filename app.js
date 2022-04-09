c87b72d5c8717de7ec5e0a0ad62acd478e694b60
Annalisa De Martino <lizmartinmusic@gmail.com>
2022-04-09 13:21:10 +0200

    small changed

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
